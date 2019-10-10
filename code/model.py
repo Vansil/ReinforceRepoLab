ee0ef16da79d53d4adc7a4ec02a5f0adcdd8a942
Nikita <nikita.bortych@gmail.com>
2019-10-10 16:14:01 +0200

    WIP

--- code/model.py
+++ code/model.py
@@ -35,28 +35,19 @@ class ReplayMemory(object):
 
 class DQN(nn.Module):
 
-    def __init__(self, h, w, outputs):
+    def __init__(self, state_features, num_actions, layers = [64,64]):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
+        self.net = nn.Sequential()
+        for i,layer in enumerate(layers):
+            if i==0:
+                self.net.add_module("Input",nn.Linear(state_features, layer))
+                self.net.add_module(f"ReLU{i}", nn.ReLU())
+            elif i==len(layers)-1:
+                self.net.add_module(f"Hidden{i}",nn.Linear(layer, num_actions))
+            else:
+                self.net.add_module("Output",nn.Linear(layer, layer))
+                self.net.add_module(f"ReLU{i}", nn.ReLU())
 
-        # Number of Linear input connections depends on output of conv2d layers
-        # and therefore the input image size, so compute it.
-        def conv2d_size_out(size, kernel_size = 5, stride = 2):
-            return (size - (kernel_size - 1) - 1) // stride  + 1
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 32
-        self.head = nn.Linear(linear_input_size, outputs)
-
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
+        return self.classifier(x)
+
