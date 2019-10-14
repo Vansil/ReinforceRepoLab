f0557dd449839bc1a2ac4df72679c82fc0cf0e88
Oskar van der Wal <oskar.vanderwal@gmail.com>
2019-10-14 17:38:53 +0200

    New experiment setup implemented

--- code/create_settings.sh
+++ code/create_settings.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
 
 # For each environment
-for env in `seq 0 2`; do
 
-    # For without and with experience replay
-    for replay in `seq 0 1`; do
+seeds_array=( 5925 3778 9685 8723 4267 7200 5413 7887 582 5476 )
+for seed in ${seeds_array[@]}; do
 
-        # Without fixed target policy or with
-        for fixed_policy in `seq 0 1`; do
+    lr_array=( 0.0001 0.001 0.01 0.1 0.5 )
+    for lr in ${lr_array[@]}; do
 
-            # For each setting for reward clipping
-            reward_array=( 0 1 100)
-            for reward_clip in ${reward_array[@]}; do
+        for env in `seq 0 2`; do
 
-                # Decide on architecture
-                for arch in `seq 0 2`; do
+            settings_array=( 0 1 2 3 4 )
+            for setting in ${settings_array[@]}; do
+                case "$setting" in
+                    0)
+                        replay=0
+                        fixed_policy=0
+                        reward_clip=0
+                        ;;
+                    1)
+                        replay=1
+                        fixed_policy=0
+                        reward_clip=0
+                        ;;
+                    2)
+                        replay=0
+                        fixed_policy=1
+                        reward_clip=0
+                        ;;
+                    3)
+                        replay=0
+                        fixed_policy=0
+                        reward_clip=1
+                        ;;
+                    4)
+                        replay=0
+                        fixed_policy=0
+                        reward_clip=100
+                        ;;
 
-                    # Write setting on line
-	                  echo --env $env --replay $replay --fixed_T_policy $fixed_policy --reward_clip $reward_clip --seed 42 --architecture $arch
+            esac
+                # Write setting on line
+	              echo --env $env --replay $replay --fixed_T_policy $fixed_policy --reward_clip $reward_clip --seed $seed
 
-                done
             done
         done
     done
